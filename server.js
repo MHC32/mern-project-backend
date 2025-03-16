14ac96e13f33c843a7c0bdbb05833da186aab45f
MHC32 <hantzmichaelchery6@gmail.com>
2025-03-16 19:50:54 -0400

    signup okay at 70%

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user.routes')
 const app = express();
 require('./config/db')
 require('dotenv').config({path: './config/.env'})
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+//routes
+app.use('/api/user', userRoutes); 
+
+
 
 
 
