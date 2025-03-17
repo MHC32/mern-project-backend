9261a7bf8bb05bf95085a76c36679ddcf1410834
MHC32 <hantzmichaelchery6@gmail.com>
2025-03-16 23:53:37 -0400

    requireAuth is okay 100 %

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken');
-const userModel = require('../models/user.model');
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model");
 
 module.exports.checkUser = async (req, res, next) => {
   const token = req.cookies.jwt;
@@ -8,19 +8,19 @@ module.exports.checkUser = async (req, res, next) => {
     jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
       if (err) {
         res.locals.user = null;
-        res.cookie('jwt', '', { maxAge: 1 });
+        res.cookie("jwt", "", { maxAge: 1 });
         next();
       } else {
         try {
           const userId = decodedToken.id;
-            console.log('decodedToken ', decodedToken)
+          console.log("decodedToken ", decodedToken);
           let user = await userModel.findById(userId);
           res.locals.user = user;
           console.log(res.locals.user);
           next();
         } catch (error) {
           res.locals.user = null;
-          res.cookie('jwt', '', { maxAge: 1 }); 
+          res.cookie("jwt", "", { maxAge: 1 });
         }
       }
     });
@@ -29,3 +29,23 @@ module.exports.checkUser = async (req, res, next) => {
     next();
   }
 };
+
+module.exports.requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+  
+    if (token) {
+      jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+        if (err) {
+          console.log(err);
+          return res.status(401).json({ message: 'Invalid token' });
+        } else {
+          console.log(decodedToken.id);
+          res.locals.userId = decodedToken.id;
+          next();
+        }
+      });
+    } else {
+      console.log('No token');
+      return res.status(401).json({ message: 'No token provided' });
+    }
+  };
