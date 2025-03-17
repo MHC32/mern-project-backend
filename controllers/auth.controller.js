d51292165bfc3aca43ebc8b3f02cafb613372cea
MHC32 <hantzmichaelchery6@gmail.com>
2025-03-17 01:40:35 -0400

    signInErrors  is okay 100 %

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const userModel = require('../models/user.model')
 const jwt = require('jsonwebtoken');
-const { signUpErrors } = require('../utils/errors.utils');
+const { signUpErrors, signInErrors } = require('../utils/errors.utils');
 
 const maxAge = 3 * 24 * 60 *  60 * 1000 ; 
 const createToken = (id) => {
@@ -33,7 +33,8 @@ module.exports.signIn = async(req, res ) => {
       res.cookie('jwt', token,  {httpOnly: true, maxAge});
       res.status(200).json({ user: user._id})
    } catch (error) {
-      res.status(200).json(error)
+      const errors = signInErrors(error);
+      res.status(200).json(errors)
    }
 }
 
--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -25,3 +25,18 @@ module.exports.signUpErrors = (err) => {
   
     return errors;
   };
+
+
+  module.exports.signInErrors = (err) => {
+    const errors = {email: '', password: ''}
+
+    if(err.message.includes('email')){
+        errors.email= 'Email inconnu'
+    }
+
+    if(err.message.includes('password')){
+        errors.password= 'Le mot de passe ne correspond pas '
+    }
+
+    return errors
+  }
