6b5883540b987e25201adcc8f52203d721457877
MHC32 <hantzmichaelchery6@gmail.com>
2025-03-18 01:33:54 -0400

    cors is okay backend finish

--- server.js
+++ server.js
@@ -3,10 +3,24 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
-const app = express();
 require('./config/db');
 const {checkUser, requireAuth} = require('./middleware/auth.middleware');
 require('dotenv').config({path: './config/.env'});
+const cors = require('cors');
+
+
+const app = express();
+
+const corsOptions  = {
+    origin: process.env.CLIENT_URL,
+    Credential: true,
+     'allowedHeaders':['sessionId', 'content-type'],
+     'exposedHeaders': ['sessionId'],
+     'methods': 'GET,PATCH,POST,DELETE,HEAD',
+     'preflighContinue':false
+}
+
+app.use(cors(corsOptions));
 
 
 app.use(bodyParser.json());
